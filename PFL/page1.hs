64e915169b530e4da01233ebe752ad63eeac8e8c
carlinhos18 <carloslisboa2005@gmail.com>
2025-09-23 11:02:46 +0100

    Finished Page 1 TP

--- PFL/page1.hs
+++ PFL/page1.hs
@@ -1,16 +1,25 @@
-import GHC.StaticPtr (StaticPtrInfo)
+import GHC.Float (sqrtFloat)
 leftHalf :: [a] -> [a]
-leftHalf x = take( div (length x ) 2) x
+leftHalf x = take ( div (length x ) 2) x
+
 rightHalf :: [b] -> [b]
 rightHalf z = drop ( div (length z) 2) z
+
 second :: [a] -> a
 second a = head (drop 1 a)
-last :: [a] -> a
-last a = head (drop (length a - 1)a)
+
+ultimo :: [a] -> a
+ultimo a = head (drop (length a - 1) a)
+
 init :: [a] -> [a]
-init a = take(length a - 1)a
+init a = take (length a - 1) a
+
 middle :: [a] -> a
-middle a = last( take ( div (length a + 1) 2) a)
+middle a = last ( take ( div (length a + 1) 2) a)
+
 checkPalindrome :: String -> Bool
-checkPalindrome a = if(a == reverse(a)) then True
-            else False
+checkPalindrome a = (a == reverse (a))
+
+triangleArea :: Float -> Float -> Float -> Float
+triangleArea a b c = sqrtFloat(s*(s-a)*(s-b)*(s-c))
+        where s = (a+b+c) / 2
